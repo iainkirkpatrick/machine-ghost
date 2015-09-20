6b3883a184b886dc1ce5ff28780bdfb5b3b06f96
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2015-09-21 10:46:25 +1200

    attempt to wire up basics, need node-gyp, will look into it later

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 console.log("running");
 
 var robot = require("robotjs");
-
-//Get the mouse position, returns an object with x and y.
-var mouse = robot.getMousePos();
-console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
-
-//Move the mouse down by 100 pixels.
-robot.moveMouse(mouse.x, mouse.y + 100);
-
-//Left click!
-robot.mouseClick();
+//
+// //Get the mouse position, returns an object with x and y.
+// var mouse = robot.getMousePos();
+// console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
+//
+// //Move the mouse down by 100 pixels.
+// robot.moveMouse(mouse.x, mouse.y + 100);
+//
+// //Left click!
+// robot.mouseClick();
